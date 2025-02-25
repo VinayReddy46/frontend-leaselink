0eb42243195f5b6c168d7f9a3aaea4993f1a2074
Your Name <yourname@example.com>
2025-02-25 10:31:04 +0530

    Updated code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import RentalProcess from '../compoments/HomeComponents/HowItsWork.jsx'
 import WhyChooseUs from '../compoments/HomeComponents/WhyChooseUs.jsx'
 import FAQSection from '../compoments/HomeComponents/FAQs.jsx'
 import TestimonialsCarousel from '../compoments/HomeComponents/ScrollingTestimonials.jsx'
+import ProductList from '../homecomponents/ProductList.jsx'
 
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
     <>
        <div className="relative z-0"> {/* Ensure Home doesn’t overlap Navbar */}
         <Carousel />
+        <ProductList/>
         <FeaturedBuildings/>
         
         <BlogPage/>
