340e23400efeb6916b06dc8b7a3eb18cde4b17eb
VinayReddy46 <vinayreddy@Busitron.com>
2025-03-12 18:44:06 +0530

    latest

--- src/redux/services/apiSlice.js
+++ src/redux/services/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.29.199:8000/api/v1",
+    baseUrl:"https://a1eb-49-43-203-163.ngrok-free.app/api/v1",
     credentials: "include",
   }),
   // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:7777' }),
